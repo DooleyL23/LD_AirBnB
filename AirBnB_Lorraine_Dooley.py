98280c50fd303ca502063768edd61fc476c5038f
DooleyL23 <sba24038@student.cct.ie>
2026-05-14 19:46:06 +0100

    commit

--- AirBnB_Lorraine_Dooley.py
+++ AirBnB_Lorraine_Dooley.py
@@ -4,7 +4,7 @@ import numpy as np
 from pathlib import Path
 
 DATA_PATH = Path(__file__).parent / "BigML_Dataset_airBnB.csv"
-LOGO_PATH = Path(__file__).parent / "airbnb_logo.png"
+LOGO_PATH = Path(__file__).parent / "airbnb__logo.png"
 
 
 import base64
